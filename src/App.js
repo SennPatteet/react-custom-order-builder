da7dfd7a1b161673c46b2a3cc02e427d2fdf577d
Senn Pateet <senn.patteet@hotmail.com>
2019-04-09 14:05:59 +0200

    fixed decimals

--- src/App.js
+++ src/App.js
@@ -7,58 +7,59 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'burger',
       total: 0,
-      amount: 0,
     }
   }
 
 
   addIng(e) {
 
-    let priceIng = parseInt(e.target.getAttribute('price'));
-    let amountIng = parseInt(e.target.getAttribute('amount'));
-    let nameIng = e.target.getAttribute('name');
+    let priceIng = parseFloat(e.target.getAttribute('price'));
 
     this.setState(
       (prevState)=>({
-        name: nameIng,
         total: prevState.total + priceIng,
-        amount: prevState.amount + amountIng
       })
     );
 
-    const order = [];
-
-    order.push(nameIng, priceIng)
-    console.log(order);
-    console.log(this.state);
   }
   render() {
+
 
     return (
       <div className="App">
         <h1>Build A Burger</h1>
 
+        {/* -------------------------------------------------------------------------- BREAD */}
         <h2>Bread</h2>
         <ul>
           <li><button>-</button>White Bread<button
           name="White Bread"
+          price={0.50}
+          amount = {1}
+          onClick={this.addIng.bind(this)}>+</button></li>
+
+          <li><button>-</button>Brown Bread<button
+          name="Brown Bread"
           price={2.00}
           amount = {1}
           onClick={this.addIng.bind(this)}>+</button></li>
-          
-          <li><button>-</button>Brown Bread<button>+</button></li>
-          <li><button>-</button>Lettuce<button>+</button></li>
         </ul>
 
+        {/* -------------------------------------------------------------------------- PATTY */}
         <h2>Patty</h2>
         <ul>
-          <li><button>-</button>Beef<button>+</button></li>
+        <li><button>-</button>Beef<button
+          name="Beef"
+          price={1.00}
+          amount = {1}
+          onClick={this.addIng.bind(this)}>+</button></li>
+
           <li><button>-</button>Chicken<button>+</button></li>
           <li><button>-</button>Veggie<button>+</button></li>
         </ul>
 
+        {/* -------------------------------------------------------------------------- TOPPINGS */}
         <h2>Toppings</h2>
         <ul>
           <li><button>-</button>Bacon<button>+</button></li>
@@ -66,6 +67,7 @@ class App extends Component {
           <li><button>-</button>Pickles<button>+</button></li>
         </ul>
 
+        {/* -------------------------------------------------------------------------- SAUCES */}
         <h2>Sauces</h2>
         <ul>
           <li><button>-</button>BBQ<button>+</button></li>
@@ -73,6 +75,7 @@ class App extends Component {
           <li><button>-</button>Ketchup<button>+</button></li>
         </ul>
 
+        {/* -------------------------------------------------------------------------- SIDE DISH */}
         <h2>Side Dish</h2>
         <ul>
           <li><button>-</button>Fries<button>+</button></li>
@@ -80,9 +83,13 @@ class App extends Component {
           <li><button>-</button>Onion Rings<button>+</button></li>
         </ul>
 
+
+
+        <h3>Total Price: {this.state.total}</h3>
       </div>
     );
   }
 }
+
 
 export default App;
