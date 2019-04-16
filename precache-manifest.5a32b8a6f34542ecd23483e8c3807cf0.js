905e58ef7d4c20dd56e34a32ab8c5bde1038d54b
Carolien <heymanscarolien@gmail.com>
2019-04-16 12:25:39 +0200

    Updates

---
+++ precache-manifest.5a32b8a6f34542ecd23483e8c3807cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cec51b2c111127d723c4",
+    "url": "/react-custom-order-builder/static/js/runtime~main.9df83c41.js"
+  },
+  {
+    "revision": "de357334dee7014e545d",
+    "url": "/react-custom-order-builder/static/js/main.0ffaf28c.chunk.js"
+  },
+  {
+    "revision": "048c070c433c6709826b",
+    "url": "/react-custom-order-builder/static/js/2.7eae1618.chunk.js"
+  },
+  {
+    "revision": "de357334dee7014e545d",
+    "url": "/react-custom-order-builder/static/css/main.f92f3d6b.chunk.css"
+  },
+  {
+    "revision": "048c070c433c6709826b",
+    "url": "/react-custom-order-builder/static/css/2.885c49ab.chunk.css"
+  },
+  {
+    "revision": "f371986ea6e5486a23ecf679b9815904",
+    "url": "/react-custom-order-builder/index.html"
+  }
+];
